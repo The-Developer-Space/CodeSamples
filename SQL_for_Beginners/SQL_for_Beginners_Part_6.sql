7ebb6f7bbd3d6c532be48b1c369020529d0336eb
Shameel Ahmed <zshameel@gmail.com>
2025-07-21 22:19:26 +0530

    Part 6 Scripts (Updated examples)

--- SQL_for_Beginners/SQL_for_Beginners_Part_6.sql
+++ SQL_for_Beginners/SQL_for_Beginners_Part_6.sql
@@ -5,9 +5,9 @@ CREATE TABLE Users (
 );
 
 --UNIQUE
-CREATE TABLE Employees (
-    EmployeeID INT PRIMARY KEY,
-    Email VARCHAR(255) CONSTRAINT UQ_Employees_Email UNIQUE
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    SKU VARCHAR(255) CONSTRAINT UQ_Products_SKU UNIQUE
 );
 
 --PRIMARY KEY
@@ -45,7 +45,16 @@ CREATE TABLE OrderDetails (
 );
 
 --Adding Constrains after table is created
-ALTER TABLE Orders ADD CONSTRAINT CHK_Orders_OrderAmount CHECK (OrderAmount > 0);
+-- Create the constrain without checking existing data
+ALTER TABLE Orders ADD CONSTRAINT CHK_Orders_Amount CHECK (Amount > 0);
+--Throws 23514: check constraint "chk_orders_amount" of relation "orders" is violated by some row
+
+-- Create the constraint without checking existing data
+ALTER TABLE Orders ADD CONSTRAINT CHK_Orders_Amount CHECK (Amount > 0) NOT VALID;
+
+--Validate constraint
+ALTER TABLE Orders VALIDATE CONSTRAINT CHK_Orders_Amount;
+
 
 --Real-World Example
 CREATE TABLE Customers (
@@ -64,3 +73,21 @@ CREATE TABLE Orders (
     OrderDate DATE CONSTRAINT DF_Orders_OrderDate DEFAULT CURRENT_DATE,
     CONSTRAINT FK_Orders_CustomerID FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
+
+--Constraint violation example
+INSERT INTO Customers (CustomerID, Email)
+VALUES (1, 'john.doe@example.com');
+INSERT INTO Customers (CustomerID, Email)
+VALUES (2, 'john.doe@example.com');
+
+-- Disable the constraint 
+ALTER TABLE Orders DISABLE CONSTRAINT CHK_Orders_Amount;
+
+--Insert data that violate the constraint
+INSERT INTO Orders (....) VALUES (.....)
+
+--Enable the constraint
+ALTER TABLE Orders ENABLE CONSTRAINT CHK_Orders_Amount;
+
+-- Drop the constraint 
+ALTER TABLE Orders DROP CONSTRAINT CHK_Orders_Amount;
